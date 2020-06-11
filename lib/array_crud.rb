eaa8acb45c9ef0ca225647c48847bdbc6b435754
Jacob G. Wazydrag <jgw.jakegeorge@gmail.com>
2020-06-11 16:48:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,8 +17,9 @@ end
 p add_element_to_end_of_array([1, 2, 3, 4], 5)
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
+p add_element_to_start_of_array([2, 3, 4, 5], 1)
 
 def remove_element_from_end_of_array(array)
   
