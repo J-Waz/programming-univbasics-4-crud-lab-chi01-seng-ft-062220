019586e4e5ffb43c239e28526386b85615879675
Jacob G. Wazydrag <jgw.jakegeorge@gmail.com>
2020-06-11 17:01:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,12 +34,12 @@ p remove_element_from_start_of_array([0, 1, 2, 3, 4, 5])
 def retrieve_element_from_index(array, index_number)
   array[index_number]
 end
-p retrieve_element_from_index([1, 2, 3, 4, 5], 0)
+p retrieve_element_from_index([1, 2, 3, 4, 5], 1)
 
 def retrieve_first_element_from_array(array)
   array[0]
 end
-p retrieve_first_element_from_array
+p retrieve_first_element_from_array([1, 2, 3, 4, 5])
 
 def retrieve_last_element_from_array(array)
   array[-1]
